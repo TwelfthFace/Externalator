edb6dd3b6c3ba2d1a4c47f8cde4e5bf59fb6a416
Jack Daniel <alcomeoh@gmail.com>
2023-10-25 13:39:16 +0100

    first commit

---
+++ externalator.py
@@ -0,0 +1,14 @@
+#!/bin/env python
+
+import argparse
+from modules import banner
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='External Infrastructure automator for usage inside PentestPeople.')
+    parser.add_argument('IP', metavar='ip', help='IP address to aim at.')
+    parser.add_argument('Port', metavar='port', type=int, help='Port to aim at.')
+    
+    args = parser.parse_args()
+
+    banner.print_banner()
