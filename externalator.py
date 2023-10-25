48814f1d774df7448c87f1a4e4d89fc4d0f9055d
TwelfthFace <alcomeoh@gmail.com>
2023-10-25 14:44:50 +0100

    parse nmap xml for ports using ssl

--- externalator.py
+++ externalator.py
@@ -4,6 +4,20 @@ import argparse
 from libnmap.parser import NmapParser
 from modules import banner
 
+def main():
+
+    with open('scope.txt') as f:
+        scope_list = [line.rstrip('\n') for line in f]
+        nmap_report = NmapParser.parse_fromfile("{}-tcpscan-0.xml".format(scope_list[0]))
+        for host in nmap_report.hosts:
+            for service in host.services:
+                if service.open() and "ssl" in service.tunnel:
+                    print(f"{service.tunnel} found for service: {service.service} on port {service.port}")
+
+
+    print("Nmap scan summary: {0}".format(nmap_report.summary))
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='External Infrastructure automator for usage inside PentestPeople.')
@@ -13,3 +27,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     banner.print_banner()
+
+    main()
