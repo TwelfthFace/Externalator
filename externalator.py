e260db662a7942772bf9a2f59ef7cd9da9e4d4c6
Jack D. <alcomeho@gmail.com>
2023-11-28 11:53:24 -0500

    clean up: could it get any cleaner???? ye

--- externalator.py
+++ externalator.py
@@ -17,7 +17,6 @@ def main():
                 print(f'{host.address} ')
                 for port in host.get_open_ports():
                    service = host.get_service(port[0], protocol=port[1])
-                   #print(f"port: {port} service: {service}")
                    em.expected_port_service(host, host.address, port)
         except Exception as e:
             errors.append(f"Invalid XML in {working_dir} : {file} : {e}")
--- modules/external_model.py
+++ modules/external_model.py
@@ -3,71 +3,63 @@ def print_None_if_empty(string):
         return None
     return string
 
+def print_service_details(nmap_host, port):
+    service = nmap_host.get_service(port[0], protocol=port[1])
+    print(f"Service Name: {print_None_if_empty(service.service)}")
+    print(f"Port: {print_None_if_empty(service.port)}")
+    print(f"Protocol: {print_None_if_empty(service.protocol)}")
+    print(f"Tunnel?: {print_None_if_empty(service.tunnel)}")
+    print(f"Banner?: {print_None_if_empty(service.banner)}")
+
 def expected_port_service(nmap_host, ip, port):
-    print('-'*100)
     try:    
         match port[0], port[1]:
             case 21, 'tcp':
-                print("")
+                print_service_details(nmap_host, port)
             case 22, 'tcp':
-                print("")
+                print_service_details(nmap_host, port)
             case 23, 'tcp':
-                print("")
+                print_service_details(nmap_host, port)
             case 25, 'tcp':
-                print("")
+                print_service_details(nmap_host, port)
             case 69, 'udp':
-                print("")
+                print_service_details(nmap_host, port)
             case 110, 'tcp':
-                print("")
+                print_service_details(nmap_host, port)
             case 143, 'tcp':
-                print("")
+                print_service_details(nmap_host, port)
             case 161, 'udp':
-                print("")
+                print_service_details(nmap_host, port)
             case 162, 'udp':
-                print("")
+                print_service_details(nmap_host, port)
             case 80, 'tcp':
-                service = nmap_host.get_service(port[0], protocol=port[1])
-                print(f"Service Name: {print_None_if_empty(service.service)}")
-                print(f"Port: {print_None_if_empty(service.port)}")
-                print(f"Protocol: {print_None_if_empty(service.protocol)}")
-                print(f"Tunnel?: {print_None_if_empty(service.tunnel)}")
-                print(f"Banner?: {print_None_if_empty(service.banner)}")
+                print_service_details(nmap_host, port)
             case 443, 'tcp':
-                service = nmap_host.get_service(port[0], protocol=port[1])
-                print(f"Service Name: {print_None_if_empty(service.service)}")
-                print(f"Port: {print_None_if_empty(service.port)}")
-                print(f"Protocol: {print_None_if_empty(service.protocol)}")
-                print(f"Tunnel?: {print_None_if_empty(service.tunnel)}")
-                print(f"Banner?: {print_None_if_empty(service.banner)}")
+                print_service_details(nmap_host, port)
             case 53, 'udp':
-                print("")
+                print_service_details(nmap_host, port)
             case 445, 'tcp':
-                print("")
-            case 389, 'tcp':
-                print("")
+                print_service_details(nmap_host, port)
+            case 388, 'tcp':
+                print_service_details(nmap_host, port)
             case 636, 'tcp':
-                print("")
+                print_service_details(nmap_host, port)
             case 135, 'tcp':
-                print("")
+                print_service_details(nmap_host, port)
             case 3389, 'tcp':
-                print("")
+                print_service_details(nmap_host, port)
             case 1433, 'tcp':
-                print("")
+                print_service_details(nmap_host, port)
             case 4022, 'tcp':
-                print("")
+                print_service_details(nmap_host, port)
             case 135, 'tcp':
-                print("")
+                print_service_details(nmap_host, port)
             case 1434, 'tcp':
-                print("")
+                print_service_details(nmap_host, port)
             case 1434, 'udp':
-                print("")
+                print_service_details(nmap_host, port)
             case _:
-                service = nmap_host.get_service(port[0], protocol=port[1])
-                print(f"Service Name: {print_None_if_empty(service.service)}")
-                print(f"Port: {print_None_if_empty(service.port)}")
-                print(f"Protocol: {print_None_if_empty(service.protocol)}")
-                print(f"Tunnel?: {print_None_if_empty(service.tunnel)}")
-                print(f"Banner?: {print_None_if_empty(service.banner)}")
+                print_service_details(nmap_host, port)
     except Exception as e:
         print(repr(e))
 
